44d6a7718d0a285362924753d54421e8f42b2c07
David Gatti <github@gatti.pl>
2018-06-13 22:06:12 +0000

    Improvements

--- index.js
+++ index.js
@@ -7,40 +7,50 @@ let moment = require('moment');
 exports.handler = async (event) => {
     
     //
-    //  1.  Get the data when we started executing the Step Function
+    //  1.  First we check if the execution data is present. If not this means 
+    //      that the SF Loop just started and we should set the data when
+    //      the execution ocoured.
+    //
+    if(!event.execution_date)
+    {
+        event.execution_date = moment().format("YYYY-MM-DD");
+    }
+    
+    //
+    //  2.  Get the data when we started executing the Step Function
     //
     let execution_date = event.execution_date;
     
     //
-    //  2.  Get the actual date
+    //  3.  Get the actual date
     //
 	let now = moment();
 	
 	//
-	//  3.  Convert the execution date in to a Moment object
+	//  4.  Convert the execution date in to a Moment object
 	//
     let past = moment(execution_date);
     
     //
-    //  4.  Add to the execution time almost a year 
+    //  5.  Add to the execution time almost a year 
     //
     let future = past.add(360, 'days');
     
     //  
-    //  5.  Check to see if the future date based on the executin time 
+    //  6.  Check to see if the future date based on the executin time 
     //      is still ahead of Now. If Now becomes bigger, then we know one year
     ///     passed.
     //
     let result = moment(future).isBefore(now);
 
     //
-    //  6.  By default we asume that we did not reach the year limit of
+    //  7.  By default we asume that we did not reach the year limit of
     //      a Step Functions
     //
     event.year_limit = false;
     
 	//
-	//  7.  Check if we are exciding our time limit of a year
+	//  8.  Check if we are exciding our time limit of a year
 	//
 	if(result)
 	{
